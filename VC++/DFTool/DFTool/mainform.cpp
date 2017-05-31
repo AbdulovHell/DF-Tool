fc2aa661cd98dc128147a61d9adf5e948089db5e
AbdulovHell <dronovova@gmail.com>
2017-05-31 11:07:50 +0300

    timewarp

--- VC++/DFTool/DFTool/mainform.cpp
+++ VC++/DFTool/DFTool/mainform.cpp
@@ -18,47 +18,46 @@ void DFTool::mainform::OpenDF()
 {
 	try {
 		if (!ml->isLoaded())
-			throw gcnew Exception("load config file first");
+			throw gcnew Exception("Load config file first");
 		DWORD aProcesses[1024], cbNeeded, cProcesses;
 		HANDLE hProcess;
 		struct _MODULEINFO modinfo;
-		DWORD v10;
+		DWORD dummy;
 		HMODULE hModule;
 		wchar_t szProcessName[MAX_PATH] = TEXT("<unknown>");
 		
-		EnumProcesses(idProcess, sizeof(idProcess), &cbNeeded);
-		cbNeeded >>= 2;
-		if (cbNeeded <= 0)
-			throw gcnew Exception("error EnumProcesses");
-		for (int i = 0; i < cbNeeded; i++) {
-			if (idProcess[i] < 5)
-				continue;
-			hDF = OpenProcess(PROCESS_ALL_ACCESS, FALSE, idProcess[i]);
-			if (hDF) {
-				if (EnumProcessModulesEx(hDF, &hModule, sizeof(HMODULE), &v10, LIST_MODULES_64BIT)) {
-					if (GetModuleBaseName(hDF, hModule, szProcessName, sizeof(szProcessName) / sizeof(TCHAR))) {
-						if ((strcmp("Dwarf Fortress.exe", szProcessName) == 0) || (strcmp("Dwarf Fortress Rus.exe", szProcessName) == 0)) {
-							if (GetModuleInformation(hDF, hModule, &modinfo,	sizeof(MODULEINFO))) {
-								StartAddr = (long long)modinfo.lpBaseOfDll;
-								hProcess = hDF;
-								hWnd = FindWindow("SDL_app", "Dwarf Fortress");
+		EnumProcesses(aProcesses, sizeof(aProcesses), &cbNeeded);
+		cProcesses = cbNeeded/sizeof(DWORD);
+		if (cProcesses <= 0)
+			throw gcnew Exception("Error EnumProcesses");
+		for (DWORD i = 0; i < cProcesses; i++) {
+			hProcess = OpenProcess(PROCESS_ALL_ACCESS, FALSE, aProcesses[i]);
+			if (hProcess) {
+				if (EnumProcessModulesEx(hProcess, &hModule, sizeof(HMODULE), &dummy, LIST_MODULES_64BIT)) {
+					if (GetModuleBaseName(hProcess, hModule, szProcessName, sizeof(szProcessName) / sizeof(TCHAR))) {
+						String^ str = gcnew String(szProcessName);
+						if (("Dwarf Fortress.exe" == str) || ("Dwarf Fortress Rus.exe" == str)) {
+							if (GetModuleInformation(hProcess, hModule, &modinfo,	sizeof(MODULEINFO))) {
+								DFStartAddr = (uint64_t)modinfo.lpBaseOfDll;
+								hDF = hProcess;
+								hDFWnd = FindWindow(L"SDL_app", L"Dwarf Fortress");
 								//TODO: Fix it for 64bit
-								long long plus = get_addr_by_name("pause");
-								pause = StartAddr + plus;
-								season = StartAddr + get_addr_by_name("season");
-								season_tick = StartAddr + get_addr_by_name("season_tick");
-								plus = get_addr_by_name("state");
-								state_addr = StartAddr + plus;
+								uint64_t PauseOffset = ml->GetAddrByName("pause");
+								PauseStateAddr = DFStartAddr + PauseOffset;
+								///season = StartAddr + get_addr_by_name("season");
+								///season_tick = StartAddr + get_addr_by_name("season_tick");
+								///plus = get_addr_by_name("state");
+								///state_addr = StartAddr + plus;
 
-								for (int i = 0; i < 11; i++)
-									Debug_func[i] = StartAddr + get_addr_by_name("debug") + i;
-								Init_DebugFunc();
+								///for (int i = 0; i < 11; i++)
+								///	Debug_func[i] = StartAddr + get_addr_by_name("debug") + i;
+								///Init_DebugFunc();
 								//if (Init_EAW())
 								//	EAW_ON = true;
 								//CheckBox1->Checked = EAW_ON;
 								//Edit1->Text = IntToStr(Init_StartDwarf());
-								state = STATE_START;
-								Init_Timewarp();
+								///state = STATE_START;
+								InitTimeWarp();
 								//read_d_init();
 								/*block_addr =
 								(__int64)StartAddr + get_addr_by_name
@@ -81,7 +80,7 @@ void DFTool::mainform::OpenDF()
 								(void*)con4_addr, con4_old, 6, NULL);
 								}
 								Init_moodAcc();     */
-								Timer1->Enabled = true;
+								///Timer1->Enabled = true;
 								return;
 							}
 							//else
@@ -96,7 +95,7 @@ void DFTool::mainform::OpenDF()
 			}
 			//else
 			//	ErrorExit(TEXT("OpenProcess"));
-			CloseHandle(v5);
+			CloseHandle(hProcess);
 		}
 		throw gcnew Exception("Dwarf Fortress.exe isn`t finded");
 	}
@@ -105,11 +104,86 @@ void DFTool::mainform::OpenDF()
 	}
 }
 
+void DFTool::mainform::InitTimeWarp()
+{
+	unsigned char NOP;
+	uint64_t addr_inj = DFStartAddr + ml->GetAddrByName("inject_point"); //+0xC
+	ReadProcessMemory(hDF, (void*)(addr_inj + 0xC), &NOP, 1, NULL);
+	if (NOP == 0x90) {
+		TimeWarpEnBtn->Enabled = false;
+		TimeWarpSetMultBtn->Enabled = true;
+		TimeWarpMultEd->Enabled = true;
+
+		unsigned int rate = 1;
+		uint64_t offset;
+		ReadProcessMemory(hDF, (void*)(addr_inj + 2), &offset, 8, NULL);
+		TimeWarpMultAddr = offset + 1;
+		ReadProcessMemory(hDF, (void*)TimeWarpMultAddr, &rate, 4, NULL);
+		TimeWarpMultEd->Text = rate.ToString();
+	}
+}
+
+void DFTool::mainform::EnableTimeWarp()
+{
+	char PauseState; //ставим паузу
+	ReadProcessMemory(hDF, (void*)PauseStateAddr, &PauseState, 1, NULL);
+	if (!PauseState) {
+		PauseState = 1;
+		WriteProcessMemory(hDF, (void*)PauseStateAddr, &PauseState, 1, NULL);
+	}
+	//
+	void *ExtCode = VirtualAllocEx(hDF, NULL, 64, MEM_COMMIT | MEM_RESERVE,	PAGE_EXECUTE_READWRITE);
+	uint64_t Inject_point = DFStartAddr + ml->GetAddrByName("inject_point");
+	// меняем исходный код
+	char modsrc[13] = { 0x48,0xB9,0,0,0,0,0,0,0,0,0xFF,0xE1,0x90 };
+	uint64_t JmpToEx = (uint64_t)ExtCode;
+	memcpy(modsrc + 2, &JmpToEx, 8);
+	WriteProcessMemory(hDF, (void*)Inject_point, modsrc, 13, NULL);
+	// внешний блок
+	char Extern_code[34] = {/*mov eax,01*/0x05,0x01,0x00,0x00,0x00,
+		/*mov*/0x48,0xB9,0,0,0,0,0,0,0,0, 			//+7	StartAddr+0x13030D4
+		/*mov rcx,eax*/0x89,0x01,
+		/*cmp eax*/0x3D,0x60,0x27,0x00,0x00,
+		/*mov rcx*/0x48,0xB9,0,0,0,0,0,0,0,0,		//+24   Inject_point+0xD
+		/*jmp rcx*/0xFF,0xE1 };
+	uint64_t temp = DFStartAddr + 0x13030D4;
+	memcpy(Extern_code + 7, &temp, 8);
+	temp = Inject_point + 0xD;
+	memcpy(Extern_code + 24, &temp, 8);
+	WriteProcessMemory(hDF, ExtCode, Extern_code, 34, NULL);
+	TimeWarpMultAddr = (uint64_t)ExtCode + 1;
+	//
+	PauseState = 0;
+	WriteProcessMemory(hDF, (void*)PauseStateAddr, &PauseState, 1, NULL);
+	TimeWarpMultEd->Enabled = true;
+	TimeWarpSetMultBtn->Enabled = true;
+	TimeWarpEnBtn->Enabled = false;
+}
+
 System::Void DFTool::mainform::mainform_Load(System::Object ^ sender, System::EventArgs ^ e)
 {
 	openINI->ShowDialog();
 	ml = gcnew MemoryLayout((char*)(void*)System::Runtime::InteropServices::Marshal::StringToHGlobalAnsi(openINI->FileName));
 	ml->isLoaded();
+}
+
+System::Void DFTool::mainform::TimeWarpSetMultBtn_Click(System::Object ^ sender, System::EventArgs ^ e)
+{
+	UInt32 mult;
+	if (System::UInt32::TryParse(TimeWarpMultEd->Text, mult))
+		WriteProcessMemory(hDF, (void*)TimeWarpMultAddr, &mult, 4, NULL);
+	else
+		;	//TODO: обработать ошибку
+}
+
+System::Void DFTool::mainform::TimeWarpEnBtn_Click(System::Object ^ sender, System::EventArgs ^ e)
+{
+	EnableTimeWarp();
+}
+
+System::Void DFTool::mainform::CnctBtn_Click(System::Object ^ sender, System::EventArgs ^ e)
+{
+	OpenDF();
 }
 
 DFTool::mainform::MemoryLayout::MemoryLayout(const char * Dest)
--- VC++/DFTool/DFTool/mainform.h
+++ VC++/DFTool/DFTool/mainform.h
@@ -44,6 +44,20 @@ namespace DFTool {
 				delete components;
 			}
 		}
+	private: System::Windows::Forms::Button^  CnctBtn;
+	protected:
+	private: System::Windows::Forms::TabControl^  tabControl1;
+	private: System::Windows::Forms::TabPage^  tabPage1;
+	private: System::Windows::Forms::TabPage^  tabPage2;
+	private: System::Windows::Forms::Button^  TimeWarpEnBtn;
+	private: System::Windows::Forms::Button^  TimeWarpSetMultBtn;
+
+	private: System::Windows::Forms::TextBox^  TimeWarpMultEd;
+
+
+
+	private: System::Windows::Forms::GroupBox^  TimeWarpControls;
+
 	private: System::Windows::Forms::OpenFileDialog^  openINI;
 	protected:
 
@@ -59,10 +73,17 @@ namespace DFTool {
 			uint64_t GetAddrByName(char* name);
 		};
 		void OpenDF();
+		void InitTimeWarp();
+		void EnableTimeWarp();
 		/// <summary>
 		/// Обязательная переменная конструктора.
 		/// </summary>
 		MemoryLayout^ ml;
+		HANDLE hDF;
+		HWND hDFWnd;
+		uint64_t DFStartAddr;
+		uint64_t TimeWarpMultAddr;
+		uint64_t PauseStateAddr;
 		System::ComponentModel::Container ^components;
 
 #pragma region Windows Form Designer generated code
@@ -73,20 +94,121 @@ namespace DFTool {
 		void InitializeComponent(void)
 		{
 			this->openINI = (gcnew System::Windows::Forms::OpenFileDialog());
+			this->CnctBtn = (gcnew System::Windows::Forms::Button());
+			this->tabControl1 = (gcnew System::Windows::Forms::TabControl());
+			this->tabPage1 = (gcnew System::Windows::Forms::TabPage());
+			this->tabPage2 = (gcnew System::Windows::Forms::TabPage());
+			this->TimeWarpEnBtn = (gcnew System::Windows::Forms::Button());
+			this->TimeWarpSetMultBtn = (gcnew System::Windows::Forms::Button());
+			this->TimeWarpMultEd = (gcnew System::Windows::Forms::TextBox());
+			this->TimeWarpControls = (gcnew System::Windows::Forms::GroupBox());
+			this->tabControl1->SuspendLayout();
+			this->TimeWarpControls->SuspendLayout();
 			this->SuspendLayout();
+			// 
+			// CnctBtn
+			// 
+			this->CnctBtn->Location = System::Drawing::Point(12, 12);
+			this->CnctBtn->Name = L"CnctBtn";
+			this->CnctBtn->Size = System::Drawing::Size(75, 35);
+			this->CnctBtn->TabIndex = 0;
+			this->CnctBtn->Text = L"Connect";
+			this->CnctBtn->UseVisualStyleBackColor = true;
+			this->CnctBtn->Click += gcnew System::EventHandler(this, &mainform::CnctBtn_Click);
+			// 
+			// tabControl1
+			// 
+			this->tabControl1->Controls->Add(this->tabPage1);
+			this->tabControl1->Controls->Add(this->tabPage2);
+			this->tabControl1->Location = System::Drawing::Point(21, 107);
+			this->tabControl1->Name = L"tabControl1";
+			this->tabControl1->SelectedIndex = 0;
+			this->tabControl1->Size = System::Drawing::Size(483, 267);
+			this->tabControl1->TabIndex = 1;
+			// 
+			// tabPage1
+			// 
+			this->tabPage1->Location = System::Drawing::Point(4, 22);
+			this->tabPage1->Name = L"tabPage1";
+			this->tabPage1->Padding = System::Windows::Forms::Padding(3);
+			this->tabPage1->Size = System::Drawing::Size(475, 241);
+			this->tabPage1->TabIndex = 0;
+			this->tabPage1->Text = L"tabPage1";
+			this->tabPage1->UseVisualStyleBackColor = true;
+			// 
+			// tabPage2
+			// 
+			this->tabPage2->Location = System::Drawing::Point(4, 22);
+			this->tabPage2->Name = L"tabPage2";
+			this->tabPage2->Padding = System::Windows::Forms::Padding(3);
+			this->tabPage2->Size = System::Drawing::Size(475, 241);
+			this->tabPage2->TabIndex = 1;
+			this->tabPage2->Text = L"tabPage2";
+			this->tabPage2->UseVisualStyleBackColor = true;
+			// 
+			// TimeWarpEnBtn
+			// 
+			this->TimeWarpEnBtn->Location = System::Drawing::Point(6, 19);
+			this->TimeWarpEnBtn->Name = L"TimeWarpEnBtn";
+			this->TimeWarpEnBtn->Size = System::Drawing::Size(75, 23);
+			this->TimeWarpEnBtn->TabIndex = 2;
+			this->TimeWarpEnBtn->Text = L"Enable";
+			this->TimeWarpEnBtn->UseVisualStyleBackColor = true;
+			this->TimeWarpEnBtn->Click += gcnew System::EventHandler(this, &mainform::TimeWarpEnBtn_Click);
+			// 
+			// TimeWarpSetMultBtn
+			// 
+			this->TimeWarpSetMultBtn->Enabled = false;
+			this->TimeWarpSetMultBtn->Location = System::Drawing::Point(161, 18);
+			this->TimeWarpSetMultBtn->Name = L"TimeWarpSetMultBtn";
+			this->TimeWarpSetMultBtn->Size = System::Drawing::Size(75, 23);
+			this->TimeWarpSetMultBtn->TabIndex = 3;
+			this->TimeWarpSetMultBtn->Text = L"Set multiplier";
+			this->TimeWarpSetMultBtn->UseVisualStyleBackColor = true;
+			this->TimeWarpSetMultBtn->Click += gcnew System::EventHandler(this, &mainform::TimeWarpSetMultBtn_Click);
+			// 
+			// TimeWarpMultEd
+			// 
+			this->TimeWarpMultEd->Enabled = false;
+			this->TimeWarpMultEd->Location = System::Drawing::Point(87, 21);
+			this->TimeWarpMultEd->Name = L"TimeWarpMultEd";
+			this->TimeWarpMultEd->Size = System::Drawing::Size(68, 20);
+			this->TimeWarpMultEd->TabIndex = 4;
+			this->TimeWarpMultEd->Text = L"1";
+			// 
+			// TimeWarpControls
+			// 
+			this->TimeWarpControls->Controls->Add(this->TimeWarpEnBtn);
+			this->TimeWarpControls->Controls->Add(this->TimeWarpSetMultBtn);
+			this->TimeWarpControls->Controls->Add(this->TimeWarpMultEd);
+			this->TimeWarpControls->Location = System::Drawing::Point(166, 12);
+			this->TimeWarpControls->Name = L"TimeWarpControls";
+			this->TimeWarpControls->Size = System::Drawing::Size(243, 49);
+			this->TimeWarpControls->TabIndex = 5;
+			this->TimeWarpControls->TabStop = false;
+			this->TimeWarpControls->Text = L"Time warp";
 			// 
 			// mainform
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->ClientSize = System::Drawing::Size(770, 441);
+			this->Controls->Add(this->TimeWarpControls);
+			this->Controls->Add(this->tabControl1);
+			this->Controls->Add(this->CnctBtn);
 			this->Name = L"mainform";
-			this->Text = L"mainform";
+			this->Text = L"DFTool";
 			this->Load += gcnew System::EventHandler(this, &mainform::mainform_Load);
+			this->tabControl1->ResumeLayout(false);
+			this->TimeWarpControls->ResumeLayout(false);
+			this->TimeWarpControls->PerformLayout();
 			this->ResumeLayout(false);
 
 		}
 #pragma endregion
 	private: System::Void mainform_Load(System::Object^  sender, System::EventArgs^  e);
+	private: System::Void TimeWarpSetMultBtn_Click(System::Object^  sender, System::EventArgs^  e);
+	private: System::Void TimeWarpEnBtn_Click(System::Object^  sender, System::EventArgs^  e);
+	private: System::Void CnctBtn_Click(System::Object^  sender, System::EventArgs^  e);
 	};
 }
